c6d82ee3bd2f7cf0c262c7e796c55ddef2333d6d
ringeringeraja <joaosan177@gmail.com>
2024-02-11 21:15:18 -0300

    feat: make api simpler

--- api/src/collections/pizza/index.ts
+++ api/src/collections/pizza/index.ts
@@ -6,74 +6,25 @@ export const pizza = defineCollection({
     icon: 'pizza',
     required: ['name'],
     properties: {
+      name: {
+        type: 'string'
+      },
       price: {
-        type: 'number',
+        type: 'number'
       },
       rating: {
-        type: 'number',
-      },
-      ingredients: {
-        type: 'array',
-        items: {
-          type: 'object',
-          properties: {
-            name: {
-              enum: [
-                'calabresa',
-                'mussarela',
-                'catupiry',
-                'frango',
-                'champignon',
-              ],
-            },
-            percentage: {
-              type: 'number',
-              minimum: 0,
-              maximum: 100,
-            },
-          },
-        },
-      },
-      name: {
-        getter: (value: any) => {
-          const suffix = value.ingredients[1]
-            ? value.ingredients[1].name
-            : 'à moda'
-
-          return `${value.ingredients[0]?.name} ${suffix}`
-        },
-      },
+        type: 'integer',
+        minimum: 1,
+        maximum: 5
+      }
     },
     presets: ['crud'],
-    formLayout: {
-      fields: {
-        name: {
-          if: {
-            operator: 'truthy',
-            term1: '_id',
-          },
-        },
-      },
-    },
   },
   functions: {
     get,
     getAll,
     insert,
     remove,
-  },
-  accessControl: {
-    roles: {
-      root: {
-        grantEverything: true,
-      },
-      guest: {
-        grant: [
-          'get',
-          'getAll',
-        ],
-      },
-    },
   },
 })
 
