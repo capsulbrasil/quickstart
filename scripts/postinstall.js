df404274e28685c806afa1a730db12351e47d12d
ringeringeraja <joaosan177@gmail.com>
2024-02-11 21:53:55 -0300

    fix: postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,17 +4,13 @@ const path = require('path')
 
 const main = async () => {
   if( fs.existsSync('sonata-api.d.ts') ) {
-    await fs.promises.rename(
-      'sonata-api.d.ts',
-      path.join('api', 'sonata-api.d.ts')
-    )
+    await fs.promises.rename('sonata-api.d.ts',
+      path.join('api', 'sonata-api.d.ts'))
   }
 
   if( fs.existsSync('waltz-ui.d.ts') ) {
-    await fs.promises.rename(
-      'waltz-ui.d.ts',
-      path.join('api', 'waltz-ui.d.ts')
-    )
+    await fs.promises.rename('waltz-ui.d.ts',
+      path.join('web', 'waltz-ui.d.ts'))
   }
 }
 
