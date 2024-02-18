b76b56683f2a324c3d1b8dc4aa482822338c0074
ringeringeraja <joaosan177@gmail.com>
2024-02-18 03:35:39 -0300

    chore: update config

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,22 +1,16 @@
-import { useApp, userRoutes, dashboardRoutes, defineOptions, AeriaMain } from 'waltz-ui'
+import { useApp, defineOptions, AeriaMain } from 'waltz-ui'
 import waltzPtbr from '@waltz-ui/i18n-ptbr'
 import { ptbr } from './i18n'
+import { routes } from './routes'
 
 import '@waltz-ui/ui/style.css'
 import 'aeria-app-layout/style.css'
 import './style/main.css'
 import './style/main.less'
 
-import { AuthWall } from 'aeria-app-layout'
-import DashboardLayout from './pages/_dashboard.vue'
-
 const options = defineOptions({
   component: AeriaMain,
-  dashboardComponent: DashboardLayout,
-  routes: [
-    userRoutes(AuthWall),
-    dashboardRoutes(DashboardLayout),
-  ],
+  routes,
   i18n: {
     current: 'pt_BR',
     locales: {
@@ -36,3 +30,4 @@ const options = defineOptions({
 useApp(options).then((app) => {
   app.mount()
 })
+
---
+++ web/src/routes.ts
@@ -0,0 +1,22 @@
+import { userRoutes, dashboardRoutes } from 'waltz-ui'
+import DashboardLayout  from './pages/_dashboard.vue'
+import { AuthWall } from 'aeria-app-layout'
+
+export const routes = [
+  userRoutes(AuthWall),
+  dashboardRoutes(DashboardLayout, [
+    {
+      path: '',
+      component: () => import('./pages/dashboard/index.vue'),
+      meta: {
+        title: 'Dashboard',
+        icon: 'gauge'
+      }
+    },
+  ]),
+  {
+    path: '/',
+    component: () => import('./pages/index.vue')
+  },
+]
+
